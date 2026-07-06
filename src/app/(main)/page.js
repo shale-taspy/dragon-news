0662b93f1b9a5ae93b7023d642d1a62960ab98bb
shale-taspy <shaleahammedtaspy007@gmail.com>
2026-07-06 09:00:46 +0600

    Done Categories left side

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,13 @@
+import Catagories from "../components/shared/Catagories";
 
 
 export default function Home() {
   return (
     <div className="grid grid-cols-12 gap-2 container mx-auto my-12">
-      <div className="font-bold text-3xl bg-green-200 col-span-3">All Catagories</div>
+      <div className="font-bold   col-span-3 ">
+        <p className="text-lg">All Catagories</p>
+        <Catagories></Catagories>
+      </div>
       <div className="font-bold text-3xl bg-red-200 col-span-6">All News</div>
       <div className="font-bold text-3xl bg-cyan-200 col-span-3">All Scoial</div>
     </div>
